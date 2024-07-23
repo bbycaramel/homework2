81d28347060741f8c2f54b56929fe4bb8a01f324
Purple Caramel <124286350+bbycaramel@users.noreply.github.com>
2024-07-23 22:11:10 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,4 @@
+number_of_homework = 12
+number_of_hours_spent = 1.5
+course_name = "Python,"
+print (course_name, "всего задач:", number_of_homework, "затрачено часов:", number_of_hours_spent, "среднее время выполнения:", (number_of_hours_spent/number_of_homework))
